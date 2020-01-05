4161fdf941b6bb3d1d39c1b4a9f9ea5e6ed632e0
RadhikaSonawane <radhika.sonawane@medieinstitutet.se>
2020-01-05 04:43:04 -0800

    add testcase

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 //import { render } from '@testing-library/react';
 import ReactDom from "react-dom";
 import App from './App';
+import Questions from "./component/Questions"
 
 /**test('renders learn react link', () => {
   const { getByText } = render(<App />);
@@ -14,3 +15,12 @@ it("Rendering without crashing", () => {
   ReactDom.render(<App></App>, div);
   ReactDom.unmountComponentAtNode(div);
 });
+
+test("Rendering Questions page", () => {
+  const div = document.createElement("div");
+  ReactDom.render(<Questions/>, div);
+  ReactDom.unmountComponentAtNode(div);
+});
+
+
+
